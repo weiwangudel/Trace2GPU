bf9ec1a200a77760ec0aa3d7971ee99057d16d52
weiwang <weiwang.udel@gmail.com>
2014-03-10 12:46:12 -0400

    script version. Upto reconstruction of sequential version.

---
+++ ReConstruct/step0.c
@@ -0,0 +1,140 @@
+Write
+134524992
+Read
+Constant43532
+Line68
+Write
+134525000
+Read
+Constant12313
+Line69
+for ( i0 = 0 ; i0 <= 99 ; i0++)
+  Write
+  134685824 + 8*i0
+  Read
+  1*i0
+  Line72
+  Write
+  134686624 + 8*i0
+  Read
+  1*i0
+  Read
+  Constant1
+  Read
+  Constant100
+  Read
+  Constant2.000000
+  Line73
+  Write
+  134688224 + 8*i0
+  Read
+  1*i0
+  Read
+  Constant1
+  Read
+  Constant100
+  Read
+  Constant4.000000
+  Line74
+  Write
+  134687424 + 8*i0
+  Read
+  1*i0
+  Read
+  Constant1
+  Read
+  Constant100
+  Read
+  Constant6.000000
+  Line75
+  Write
+  134689824 + 8*i0
+  Read
+  1*i0
+  Read
+  Constant1
+  Read
+  Constant100
+  Read
+  Constant8.000000
+  Line76
+  Write
+  134690624 + 8*i0
+  Read
+  1*i0
+  Read
+  Constant1
+  Read
+  Constant100
+  Read
+  Constant9.000000
+  Line77
+  Write
+  134685024 + 8*i0
+  Read
+  Constant0.000000
+  Line78
+  Write
+  134689024 + 8*i0
+  Read
+  Constant0.000000
+  Line79
+  for ( i1 = 0 ; i1 <= 99 ; i1++)
+    Write
+    134525024 + 800*i0 + 8*i1
+    Read
+    1*i0
+    Read
+    1*i1
+    Read
+    Constant100
+    Line82
+for ( i0 = 0 ; i0 <= 99 ; i0++)
+  for ( i1 = 0 ; i1 <= 99 ; i1++)
+    Write
+    134525024 + 800*i0 + 8*i1
+    Read
+    134525024 + 800*i0 + 8*i1
+    Read
+    134685824 + 8*i0
+    Read
+    134688224 + 8*i1
+    Read
+    134686624 + 8*i0
+    Read
+    134687424 + 8*i1
+    Line125
+for ( i0 = 0 ; i0 <= 99 ; i0++)
+  for ( i1 = 0 ; i1 <= 99 ; i1++)
+    Write
+    134685024 + 8*i0
+    Read
+    134685024 + 8*i0
+    Read
+    134525000
+    Read
+    134525024 + 8*i0 + 800*i1
+    Read
+    134689824 + 8*i1
+    Line131
+for ( i0 = 0 ; i0 <= 99 ; i0++)
+  Write
+  134685024 + 8*i0
+  Read
+  134685024 + 8*i0
+  Read
+  134690624 + 8*i0
+  Line136
+for ( i0 = 0 ; i0 <= 99 ; i0++)
+  for ( i1 = 0 ; i1 <= 99 ; i1++)
+    Write
+    134689024 + 8*i0
+    Read
+    134689024 + 8*i0
+    Read
+    134524992
+    Read
+    134525024 + 800*i0 + 8*i1
+    Read
+    134685024 + 8*i1
+    Line142
